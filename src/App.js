fd2aa8614d85017ded4b600f4e25379b112be0dd
jesse <jsoldat@hotmail.com>
2017-03-17 19:06:39 +0700

    basic app is set up for testing

--- src/App.js
+++ src/App.js
@@ -22,12 +22,51 @@ class App extends Component {
       item: ''
     });
   };
-  
+
   render() {
+    const submitDisabled = !this.state.item;
     return (
-      <div className="App">
-        
-      </div>
+      <div className="ui text container" id='app'>
+        <table className='ui selectable structured large table'>
+          <thead>
+            <tr>
+              <th>Items</th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+            this.state.items.map((item,idx) => (
+              <tr key={idx}>
+                <td>{item}</td>
+              </tr>
+            ))
+          }
+          </tbody>
+          <tfoot>
+            <tr>
+              <th>
+                <form className='ui form'
+                      onSubmit={this.addItem}
+                >
+                <div className='field'>
+                  <input className='prompt'
+                        type='text'
+                        placeholder='Add item...'
+                        value={this.state.item}
+                        onChange={this.onItemChange}
+                  />
+                </div>
+                <button className='ui button'
+                      type='submit'
+                      disabled={submitDisabled}
+                > Add Item
+                </button>
+                </form>
+              </th>
+            </tr>
+          </tfoot>
+        </table>
+      </div>  
     );
   }
 }
